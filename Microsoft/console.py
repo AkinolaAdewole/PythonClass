1a649a3ee4f892a6b34a27c983a87493d24946cd
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-13 16:50:37 +0100

    Commit

--- Microsoft/console.py
+++ Microsoft/console.py
@@ -3,3 +3,8 @@ print("show this in the console")
 sum = 1 + 2 # 3
 product = sum * 2
 print(product)
+
+x,y,z = 10,20,30
+print(x)
+print(y)
+print(z)
--- Microsoft/new.py
+++ Microsoft/new.py
@@ -1,3 +1,9 @@
 # print("Hello World") 
 sum = 1 + 2
 print(sum)
+
+# Enter code below
+parsecs_input = input("Input number of parsecs:")
+parsecs = int(parsecs_input)
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + ' lightyears')
