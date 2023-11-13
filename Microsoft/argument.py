016795d78e2b074628d3b809cfb7f4fa01098353
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-13 11:17:21 +0100

    first commit

---
+++ Microsoft/argument.py
@@ -0,0 +1,7 @@
+import sys
+
+['backup.py', '2023-01-01'] 
+
+print(sys.argv)
+print(sys.argv[0]) # program name
+print(sys.argv[1]) # first arg
--- Microsoft/date.py
+++ Microsoft/date.py
@@ -3,3 +3,8 @@ date.today()
 print(date.today())
 
 print("Today's date is: " + str(date.today()))
+
+
+parsecs = 11
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears" )
