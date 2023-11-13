f343015bfdeac367fd54785923105aaa22cdba54
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-13 18:49:41 +0100

    Commit

---
+++ Educative/print.py
@@ -0,0 +1,10 @@
+print(50, 1000, 3.142, "Hello World")
+
+# By default, each print statement prints text in a new line. 
+# If we want multiple print statements to print in the same line, 
+# we can use the following code:
+print("Hello", end="")
+print("World")
+
+print("Hello", end=" ")
+print("World")
--- Microsoft/new.py
+++ Microsoft/new.py
@@ -7,3 +7,5 @@ parsecs_input = input("Input number of parsecs:")
 parsecs = int(parsecs_input)
 lightyears = parsecs * 3.26
 print(str(parsecs) + " parsecs is " + str(lightyears) + ' lightyears')
+
+
