78b3bfbd5d679d1d915b27f8b1344d880a39855f
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-14 19:56:26 +0100

    Python

---
+++ HelloWorld/ifStatement.py
@@ -0,0 +1,7 @@
+temperature =15
+if temperature >30 :
+    print("It is a hot day, drink plenty of water")
+elif temperature>20 :
+    print("It is nice day")
+else :
+    print("Weather is cold")
--- HelloWorld/logicalOperator.py
+++ HelloWorld/logicalOperator.py
@@ -4,3 +4,8 @@
 # OR or (returns True if at least one condition is True)
 # NOT not (returns the opposite Boolean value)
 
+price = 20
+print(price > 10 and price < 30)
+print(price > 10 or price < 30)
+print(not price < 30)
+print(not price < 10)
