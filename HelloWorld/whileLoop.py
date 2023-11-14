3fb623a0f4f29df134d21293bca8027ab603847c
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-14 22:54:16 +0100

    Python

---
+++ HelloWorld/whileLoop.py
@@ -0,0 +1,4 @@
+# The while loop continuously checks the condition before each iteration.
+# If the condition is true, the code block inside the loop is executed.
+# When the condition becomes false, the loop stops, and the program continues with the
+# next section of code after the while block.
