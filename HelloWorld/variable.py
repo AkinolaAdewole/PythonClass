ca053608416f0a3783a3d52007db1261e9a8127a
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-14 17:24:07 +0100

    Python

---
+++ HelloWorld/variable.py
@@ -0,0 +1,10 @@
+
+# int() - converting a value to number
+# str() - converting a value to string
+# float() - for decimal e.g 3.1
+# bool() - for true or false
+
+num1 = float(input("Enter first value "))
+num2 = float(input("Enter second value "))
+sum = num1 + num2
+print("Sum is " + str(sum))
