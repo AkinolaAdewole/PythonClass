9f6b49f9164c081816d4c36852ac3f21c5a36401
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-14 23:08:32 +0100

    Python

--- HelloWorld/whileLoop.py
+++ HelloWorld/whileLoop.py
@@ -2,3 +2,14 @@
 # If the condition is true, the code block inside the loop is executed.
 # When the condition becomes false, the loop stops, and the program continues with the
 # next section of code after the while block.
+
+i = 1
+while i <= 3 :
+    print(i)
+    i = i+1
+
+
+j = 1
+while j <= 3 :
+    print(j * "*")
+    j = j+1
