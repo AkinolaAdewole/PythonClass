506a3adef1e9592d5cfbce994d37a8ecb7100732
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-15 12:18:17 +0100

    Python

---
+++ HelloWorld/rangeFunction.py
@@ -0,0 +1,11 @@
+num1 = range(5)
+for number in num1 :
+    print(number)
+
+num1 = range(5, 10)
+for number in num1 :
+    print(number)
+
+num1 = range(10, 20, 2)
+for number in num1 :
+    print(number)
