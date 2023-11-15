2e41b2abf2b76bb04fbd24a6b9eb169c68b8edec
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-15 15:54:54 +0100

    Python

---
+++ HelloWorld/tuples.py
@@ -0,0 +1,6 @@
+# In Python, a tuple is an ordered collection of elements, similar to a list.
+# However, tuples are immutable, meaning once they are created, their contents cannot be changed.
+# They are represented by parentheses () and can contain elements of different data types,
+# including numbers, strings, lists, or other tuples.
+
+num = (0, 1, 2, 3, 4,)
