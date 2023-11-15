468bfa82b2be4373d89773c810251316a8c1c62a
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-15 11:54:27 +0100

    Python

--- HelloWorld/listMethods.py
+++ HelloWorld/listMethods.py
@@ -5,3 +5,11 @@ print(num1)
 num2 = [0, 1, 2, 3, 4, 5]
 num2.append(6)
 print(num2)
+
+num3 = [0, 1, 2, 3, 4, 5]
+num3.insert(0, "A")
+print(num3)
+
+num4 = [0, 1, 2, 3, 4, 5]
+print(2 in num4)
+print(8 in num4)
