07c63e7e7988e4835fd978805edf57c0503af7c3
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-21 23:40:22 +0100

    Python Playground

--- HelloWorld/string.py
+++ HelloWorld/string.py
@@ -7,5 +7,9 @@ print(course.lower());
 
 # find(substring): Finds the index of the first occurrence of a substring; returns -1 if not found.
 print(course.find("n"))
+
+# replace(old, new): Replaces all occurrences of the old substring with a new one.
 print(course.replace("for","4"))
+
+# 'substring' in string: Checks if a substring exists in the string; returns True or False.
 print('Python' in course)
