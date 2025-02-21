b8238ef0dc36a7a119a4f32959ece83fc72be282
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-21 23:39:04 +0100

    Python Playground

--- HelloWorld/string.py
+++ HelloWorld/string.py
@@ -1,7 +1,11 @@
 course = "Python for Beginners"
 # upper(): Converts all characters in the string to uppercase.
-print(course.upper())
-print(course.lower())
+print(course.upper());
+
+# lower(): Converts all characters to lowercase.
+print(course.lower());
+
+# find(substring): Finds the index of the first occurrence of a substring; returns -1 if not found.
 print(course.find("n"))
 print(course.replace("for","4"))
 print('Python' in course)
