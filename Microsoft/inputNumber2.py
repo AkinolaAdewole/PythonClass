52a977449ddacfe73bad2ef370d7065f8e2cb098
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-22 00:02:13 +0100

    Python Playground

--- Microsoft/inputNumber2.py
+++ Microsoft/inputNumber2.py
@@ -1,4 +1,12 @@
+# Printing a message indicating this is a calculator program.
 print("calculator program")
+
+# Taking input from the user for the first number.
 first_number = input("first number: ")
+
+# Taking input from the user for the second number.
 second_number = input("second number: ")
-print(int(first_number) + int(second_number))
+
+# Converting the input values (which are strings) to integers and adding them.
+# The 'int()' function converts the string inputs to integers for arithmetic operations.
+print(int(first_number) + int(second_number))  # Printing the result of the addition.
