ede7651d72e2c382c3819aee4a40548d2724e4ed
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-21 23:29:37 +0100

    Python Playground

--- HelloWorld/rangeFunction.py
+++ HelloWorld/rangeFunction.py
@@ -18,3 +18,36 @@ for number in num1:
 # 2
 # 3
 # 4
+
+# Loop 3: Iterating over a range from 5 to 9.
+num1 = range(5, 10)
+for number in num1:
+    print(number)
+# Output:
+# 5
+# 6
+# 7
+# 8
+# 9
+
+# Loop 4: Iterating over a range from 10 to 18, with a step of 2.
+num1 = range(10, 20, 2)
+for number in num1:
+    print(number)
+# Output:
+# 10
+# 12
+# 14
+# 16
+# 18
+
+# Loop 4: Iterating over a range from 10 to 18, with a step of 2.
+num1 = range(10, 20, 2)
+for number in num1:
+    print(number)
+# Output:
+# 10
+# 12
+# 14
+# 16
+# 18
