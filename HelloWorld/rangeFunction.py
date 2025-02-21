70283fb84e5db81109f5d03c58513d47c00b0194
Akinola Adewole <adewoleakinola24@gmail.com>
2025-02-21 23:26:25 +0100

    Python Playground

--- HelloWorld/rangeFunction.py
+++ HelloWorld/rangeFunction.py
@@ -1,14 +1,20 @@
-for number in range(5) :
+# Loop 1: Iterating over a range of numbers from 0 to 4 (5 is not included).
+for number in range(5):
     print(number)
+# Output:
+# 0
+# 1
+# 2
+# 3
+# 4
 
+# Loop 2: Assigning range(5) to a variable and iterating over it.
 num1 = range(5)
-for number in num1 :
+for number in num1:
     print(number)
-
-num1 = range(5, 10)
-for number in num1 :
-    print(number)
-
-num1 = range(10, 20, 2)
-for number in num1 :
-    print(number)
+# Output:
+# 0
+# 1
+# 2
+# 3
+# 4
